c9c6573db80788e1845900198589ca2f9259abe4
AnkitDhattarwal <ankitdhatterwal09@gmail.com>
2026-07-26 08:28:35 +0530

    Initial commit

---
+++ MoiveApp/MoiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoiveAppApp.swift
+//  MoiveApp
+//
+//  Created by Ankit on 26/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MoiveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
